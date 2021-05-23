2dd38f48494549badc95a3b05c0f95a822507916
scientist-oriented <moon910904@gmail.com>
2021-05-23 10:28:40 +0900

    최종 템플릿 업로드

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
-    port: '3306',
-    password: '',
-    database: ''
+    host: 'process.env.MYSQL_HOST',
+    user: 'process.env.MYSQL_USER',
+    port: 'process.env.MYSQL_PORT',
+    password: 'process.env.MYSQL_PASSWORD',
+    database: 'process.env.MYSQL_DB_NAME'
 });
 
 module.exports = {
