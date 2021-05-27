5296ce11efb9ba55791220c996fbd1824a241ea5
System Administrator <moon910904@gmail.com>
2021-05-27 12:23:53 +0900

    util.js 누락되서 다시 업로드

--- config/util.js
+++ config/util.js
@@ -1,4 +1,4 @@
-exports.lpad = function(str, padLen, padStr) {
+const lpad = function(str, padLen, padStr) {
     if (padStr.length > padLen) {
         console.log("오류 : 채우고자 하는 문자열이 요청 길이보다 큽니다");
         return str;
@@ -22,3 +22,13 @@ exports.arrayToString = function(array) {
     str = str.slice(0, -1)
     return str
 }
+
+exports.makeCursor = function(unixTimestamp, idx) {
+    if(!unixTimestamp) {
+        return "999999999999999999999999999999"
+    } else {
+        const cursor1 = lpad(unixTimestamp.unixTimestamp, 15, 0)
+        const cursor2 = lpad(idx, 15, 0)
+        return cursor1 + cursor2
+    }     
+};
